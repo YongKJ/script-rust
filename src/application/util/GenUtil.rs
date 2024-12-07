7f9635dff7c39bb1993b8c2a6d9f5cf4c959e8db
dxj <dxj1718874198@gmail.com>
2024-12-07 21:51:28 +0800

    [update]Demo test

--- src/application/util/GenUtil.rs
+++ src/application/util/GenUtil.rs
@@ -78,19 +78,19 @@ pub fn getYamlByContent() -> String {
 }
 
 pub fn toHump(name: &str) -> String {
+    let subName = &name[1..name.len()];
     let regex = Regex::new("\\-(\\w)").unwrap();
-    let subName = name[1..name.len()].to_string();
-    let firstUpperLetter = name[0..1].to_string().to_uppercase();
+    let firstUpperLetter = name[0..1].to_uppercase();
 
-    firstUpperLetter + regex.replace_all(subName.as_str(), |lstMatch: &Captures|
+    firstUpperLetter + regex.replace_all(subName, |lstMatch: &Captures|
         lstMatch.get(1).unwrap().as_str().replace("-", "").to_uppercase()).as_ref()
 }
 
 pub fn toLine(name: &str) -> String {
+    let subName = &name[1..name.len()];
     let regex = Regex::new("([A-Z])").unwrap();
-    let subName = name[1..name.len()].to_string();
-    let firstLowerLetter = name[0..1].to_string().to_lowercase();
+    let firstLowerLetter = name[0..1].to_lowercase();
 
-    firstLowerLetter + regex.replace_all(subName.as_str(), |lstMatch: &Captures|
+    firstLowerLetter + regex.replace_all(subName, |lstMatch: &Captures|
         "-".to_string() + lstMatch.get(1).unwrap().as_str().to_lowercase().as_str()).as_ref()
 }
