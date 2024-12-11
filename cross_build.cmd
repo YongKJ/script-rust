27aadf8e938430d48c844679877c5f4e005f2751
dxj <dxj1718874198@gmail.com>
2024-12-11 21:47:57 +0800

    [update]Script build

--- cross_build.cmd
+++ cross_build.cmd
@@ -1,18 +1,28 @@
 
 :: SET DOCKER_HOST=tcp://192.168.3.25:2375
 
+:: cargo build --target=armv7-linux-androideabi
+
+:: cargo build --target=arm-linux-androideabi
+
+:: cargo build --target=aarch64-linux-android
+
 :: cargo build --target=arm-unknown-linux-gnueabihf
 
 :: cargo build --target=aarch64-unknown-linux-gnu
 
 :: cargo build --target=aarch64-unknown-linux-musl
 
-cargo build --target=x86_64-unknown-linux-gnu
+:: cargo build --target=x86_64-unknown-linux-gnu
 
 :: cargo build --target=x86_64-unknown-linux-musl
 
 :: cargo build --target=i686-unknown-linux-musl
 
+cargo build --target=x86_64-pc-windows-msvc
+
 :: cargo build --target=x86_64-pc-windows-gnu
+
+:: cargo build --target=i686-pc-windows-msvc
 
 :: cargo build --target=i686-pc-windows-gnu
