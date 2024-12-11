c47d49711c7e77a7658f11d8d21c6e358c4a24ee
dxj <dxj1718874198@gmail.com>
2024-12-11 21:25:49 +0800

    [update]Script build

--- cross_build.cmd
+++ cross_build.cmd
@@ -11,6 +11,8 @@ cargo build --target=x86_64-unknown-linux-gnu
 
 :: cargo build --target=x86_64-unknown-linux-musl
 
+:: cargo build --target=i686-unknown-linux-musl
+
 :: cargo build --target=x86_64-pc-windows-gnu
 
 :: cargo build --target=i686-pc-windows-gnu
