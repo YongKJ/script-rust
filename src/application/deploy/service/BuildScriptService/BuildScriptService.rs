c893cf9d3e8021f64e55755f52795e961048b264
dxj <1718874198@qq.com>
2024-12-12 15:24:12 +0800

    [update]Script build

--- src/application/deploy/service/BuildScriptService/BuildScriptService.rs
+++ src/application/deploy/service/BuildScriptService/BuildScriptService.rs
@@ -1,21 +1,27 @@
 use crate::application::deploy::pojo::po::OsInfo::OsInfo;
+use crate::application::deploy::pojo::po::Script::Script;
 use crate::application::pojo::dto::Log::Log;
 use crate::application::util::{DataUtil, LogUtil};
 
 pub struct BuildScriptService {
     osInfos: Vec<OsInfo>,
+    scripts: Vec<Script>
 }
 
 impl BuildScriptService {
     pub fn new() -> Self {
         Self {
-            osInfos: OsInfo::gets()
+            osInfos: OsInfo::gets(),
+            scripts: Script::gets()
         }
     }
 
     fn test(&self) {
         LogUtil::loggerLine(Log::of("BuildScriptService", "test", "self.osInfos.len()", self.osInfos.len()));
         LogUtil::loggerLine(Log::of("BuildScriptService", "test", "osInfos", DataUtil::objToJson(&self.osInfos)));
+        println!();
+        LogUtil::loggerLine(Log::of("BuildScriptService", "test", "self.scripts.len()", self.scripts.len()));
+        LogUtil::loggerLine(Log::of("BuildScriptService", "test", "scripts", DataUtil::objToJson(&self.scripts)));
     }
 }
 
