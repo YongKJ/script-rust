e6d9a143c35516660481809cac83e1c4852b15fe
dxj <1718874198@qq.com>
2024-12-19 10:04:42 +0800

    [update]Script build

--- src/application/util/PromptUtil.rs
+++ src/application/util/PromptUtil.rs
@@ -1,5 +1,3 @@
-use crate::application::deploy::pojo::po::Script::Script;
-
 pub fn packageRustScript(crossBuildPath: &str) -> (&str, Vec<&str>) {
     if cfg!(windows) {
         return ("busybox", vec![
